3fb64effb09002d0808912e6b2f439002f4f7b01
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-03 11:48:45 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 
 $(() => {
 	const $container = $("#container")
-    console.log($container)
 
+    // creating h1 element
     const $h1 = $("<h1>")
     $h1.text('HOGWARTS')
     $container.append($h1)
-    console.log($h1)
+
+    // creating h2 elelemtn with my name
+    const $h2 = $('<h2>').text('Amrita')
+    $container.append($h2)
+
+    // h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin )
+    const $h3 = $('<h3>').text('Hufflepuff')
+    $container.append($h3)
+
+    //h4 element with your pet's name
+    // this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+    const $h4 = $('<h4>').text('Kimi')
+    $h4.addClass('cat')
+    $container.append($h4)
+
+    //h4 element with your wand 
+    const $h4wand = $('<h4>').text('Holly Wand with Unicorn Hair Core')
+    $container.append($h4wand)
+    
+
+
+
+
 
 });
