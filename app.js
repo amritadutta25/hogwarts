235330a4e37922594087d7f13b7c1f89caecb7fe
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-03 12:08:03 -0500

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 
 $(() => {
+
+    //----------------
+    //Year 1
+    //----------------
 	const $container = $("#container")
 
     // creating h1 element
     const $h1 = $("<h1>")
     $h1.text('HOGWARTS')
     $container.append($h1)
+
+    //----------------
+    //Year 2
+    //----------------
 
     // creating h2 elelemtn with my name
     const $h2 = $('<h2>').text('Amrita')
@@ -24,7 +32,38 @@ $(() => {
     //h4 element with your wand 
     const $h4wand = $('<h4>').text('Holly Wand with Unicorn Hair Core')
     $container.append($h4wand)
+
+    //----------------
+    //Year 3
+    //----------------
     
+    //creating ul element (with the attribute of storage and a value of trunk)
+    const $ul = $('<ul>')
+    $ul.attr('storage', 'trunk')
+    $container.append($ul)
+
+    // adding list items
+    const $li1 = $('<li>').text('Butter beer')
+    $ul.append($li1)
+
+    const $li2 = $('<li>').text('invisibility cloak')
+    $li2.addClass("secret")
+    $ul.append($li2)
+
+    const $li3 = $('<li>').text('magic map')
+    $li3.addClass("secret")
+    $ul.append($li3)
+
+    const $li4 = $('<li>').text('time turner')
+    $li4.addClass("secret")
+    $ul.append($li4)
+
+    const $li5 = $('<li>').text('leash')
+    $li5.addClass("cat")
+    $ul.append($li5)
+
+    const $li6 = $('<li>').text("Bertie Bott's Every Flavor [Jelly] Beans")
+    $ul.append($li6)
 
 
 
